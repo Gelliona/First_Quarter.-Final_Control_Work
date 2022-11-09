4720363ef048a68952bb57bfd7ae8fb31a7f72e7
Gelliona <Gelliona81@mail.ru>
2022-11-09 22:37:25 +0400

    Program.cs added

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+
+Console.Write("Введите слова, числа или наборы символов через запятую: ");
+string inputString = Console.ReadLine()!;
+
+string[] array = ParseToArray(inputString);
+Console.WriteLine("[" + string.Join((", "), array) + "]");
+
+string[] newArray = ThreeSymbolsElementArray(array);
+Console.WriteLine("[" + string.Join(", ", newArray) + "]");
+
+
+
+string[] ParseToArray(string str)            
+{
+    string[] stringArray = str.Split(",");
+    string[] result = new string[stringArray.Length];
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = stringArray[i];
+    }
+
+    return result;
+}
+
+string[] ThreeSymbolsElementArray(string[] inputArray)
+{
+    int newSize = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+            newSize++;
+    }
+
+    string[] resultArray = new string[newSize];
+    
+    int j = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[j] = inputArray[i];
+            j++;
+        }        
+    }
+    return resultArray;
+}
